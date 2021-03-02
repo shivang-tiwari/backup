a4e6cefefa23aa08db71eb62b75e0a27e46fd11d
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2021-03-02 23:51:00 +0530

    Add files via upload

---
+++ codeforces.sh
@@ -0,0 +1,8 @@
+nohup google-chrome https://codeforces.com/ &
+disown
+rm /home/shivang/nohup.out
+rm -r /home/shivang/Projects/C++/Contest
+mkdir /home/shivang/Projects/C++/Contest
+nohup geany &
+disown
+exit
