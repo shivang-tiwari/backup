50e80428faac9e7040aa87a41afed4b07368af0d
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2021-03-03 10:59:01 +0530

    Delete codeforces.sh

--- codeforces.sh
+++
@@ -1,8 +0,0 @@
-nohup google-chrome https://codeforces.com/ &
-disown
-rm /home/shivang/nohup.out
-rm -r /home/shivang/Projects/C++/Contest
-mkdir /home/shivang/Projects/C++/Contest
-nohup geany &
-disown
-exit
