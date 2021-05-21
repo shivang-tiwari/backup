0abfac9bf915ec18f45547206333d2661ad26aef
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2021-05-21 12:19:45 +0530

    Add files via upload

--- Commands/codeforces_parse.sh
+++ Commands/codeforces_parse.sh
@@ -2,13 +2,17 @@
 cd $HOME
 cd ./cf/contest/
 cd $(ls -d */|head -n 1)
+mkdir "/home/shivang/Projects/C++/Samples/Outputs"
 for problem in * ; do
 	cd "$problem"
 	mkdir "/home/shivang/Projects/C++/Samples/${problem^^}"
+	touch "/home/shivang/Projects/C++/Samples/${problem^^}/output.txt"
+	mkdir "/home/shivang/Projects/C++/Samples/Outputs/${problem^^}"
 	for input in in* ; do
-		echo $input
 		cp $input /home/shivang/Projects/C++/Samples/${problem^^}/
-		#cp "$input /home/shivang/Projects/C++/Contest/Samples/${problem^^}/"
+	done
+	for input in ans* ; do
+		cp $input /home/shivang/Projects/C++/Samples/Outputs/${problem^^}/
 	done
 	cd -
 done
--- Commands/make_samples.sh
+++ Commands/make_samples.sh
@@ -4,14 +4,17 @@ name=$1
 if [[ -n "$name" ]]; then
 	if [ ! -d "/home/shivang/Projects/C++/Samples/$name/" ]; then
 		mkdir "/home/shivang/Projects/C++/Samples/$name/"
+		mkdir "/home/shivang/Projects/C++/Samples/Outputs/$name/"
 	fi
 	cd "/home/shivang/Projects/C++/Samples/$name/"
 	numfiles=$(ls -1 | grep -v '/$' | wc -l)
 
 
 	numfiles=$((numfiles+1))
-	touch "/home/shivang/Projects/C++/Samples/$name/mytest$numfiles.txt" &
-	geany "/home/shivang/Projects/C++/Samples/$name/mytest$numfiles.txt" &
+	touch "/home/shivang/Projects/C++/Samples/$name/in$numfiles.txt" &
+	touch "/home/shivang/Projects/C++/Samples/Outputs/$name/ans$numfiles.txt" &
+	geany "/home/shivang/Projects/C++/Samples/Outputs/$name/ans$numfiles.txt" &
+	geany "/home/shivang/Projects/C++/Samples/$name/in$numfiles.txt" &
 else
     echo "No file name provided"
 fi
