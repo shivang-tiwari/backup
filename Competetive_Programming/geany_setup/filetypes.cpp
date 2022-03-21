982e1a3a4d6c77e7881cb8789dbd82c7618b3f59
Shivang Tiwari <52480535+shivang-tiwari@users.noreply.github.com>
2022-03-21 22:12:12 +0530

    Add files via upload

--- Competetive_Programming/geany_setup/filetypes.cpp
+++ Competetive_Programming/geany_setup/filetypes.cpp
@@ -4,8 +4,8 @@
 
 [keywords]
 # all items must be in one line
-primary=alignas alignof and and_eq asm auto bitand bitor bool break case catch char char16_t char32_t class compl const const_cast constexpr continue decltype default delete do double dynamic_cast else enum explicit export extern false final float for friend goto if inline int long mutable namespace new noexcept not not_eq nullptr operator or or_eq override private protected public register reinterpret_cast return short signed sizeof static static_assert static_cast struct switch template this thread_local throw true try typedef typeid typename union unsigned using virtual void volatile wchar_t while xor xor_eq ll ld vector map set multiset stack queue string pair unordered_set
-secondary=shivang Shivang Mint
+primary=alignas alignof and and_eq asm auto bitand bitor bool break case catch char char16_t char32_t class compl const const_cast constexpr continue decltype default delete do double dynamic_cast else enum explicit export extern false final float for friend goto if inline int long mutable namespace new noexcept not not_eq nullptr operator or or_eq override private protected public register reinterpret_cast return short signed sizeof static static_assert static_cast struct switch template this thread_local throw true try typedef typeid typename union unsigned using virtual void volatile wchar_t while xor xor_eq ll ld vector map set multiset stack queue string pair unordered_set ordered_set function tuple deque
+secondary=shivang Shivang Mint debug booga shivang_ka_laptop
 # these are the Doxygen keywords
 docComment=a addindex addtogroup anchor arg attention author authors b brief bug c callergraph callgraph category cite class code cond copybrief copydetails copydoc copyright date def defgroup deprecated details dir dontinclude dot dotfile e else elseif em endcode endcond enddot endhtmlonly endif endinternal endlatexonly endlink endmanonly endmsc endrtfonly endverbatim endxmlonly enum example exception extends file fn headerfile hideinitializer htmlinclude htmlonly if ifnot image implements include includelineno ingroup interface internal invariant latexonly li line link mainpage manonly memberof msc mscfile n name namespace nosubgrouping note overload p package page par paragraph param post pre private privatesection property protected protectedsection protocol public publicsection ref related relatedalso relates relatesalso remark remarks result return returns retval rtfonly sa section see short showinitializer since skip skipline snippet struct subpage subsection subsubsection tableofcontents test throw throws todo tparam typedef union until var verbatim verbinclude version warning weakgroup xmlonly xrefitem
 
@@ -31,7 +31,7 @@ comment_close=*/
 
 # set to false if a comment character/string should start at column 0 of a line, true uses any
 # indentation of the line, e.g. setting to true causes the following on pressing CTRL+d
-	#command_example();
+#command_example();
 # setting to false would generate this
 #	command_example();
 # This setting works only for single line comments
@@ -50,17 +50,17 @@ context_action_cmd=
 # %e will be replaced by the filename without extension
 # (use only one of it at one time)
 FT_00_LB=_Compile
-FT_00_CM=g++ -std=c++17 -Wshadow -Wall -o "%e" "%f" -O2 -Wno-unused-result
+FT_00_CM=/home/shivang/Projects/Commands/cp_compile.sh "%e" "%f"
 FT_00_WD=
-FT_01_LB=_Build
-FT_01_CM=g++ -std=c++17 -Wshadow -Wall -o "%e" "%f" -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG
+FT_01_LB=Submit
+FT_01_CM=gnome-terminal --window-with-profile cp_submit -- /home/shivang/Projects/Commands/submit.sh "%e"
 FT_01_WD=
 FT_02_LB=_Lint
-FT_02_CM=cppcheck --language=c++ --enable=warning,style --template=gcc "%f"
+FT_02_CM=clang-tidy -checks=-*,clang-analyzer-*,-clang-analyzer-cplusplus* "%f" --
 FT_02_WD=
 EX_00_LB=_Execute
 EX_00_CM="./%e"
 EX_00_WD=
-EX_01_LB=Execute with Testcase
-EX_01_CM=time "./%e" < in.txt
+EX_01_LB=
+EX_01_CM=
 EX_01_WD=
